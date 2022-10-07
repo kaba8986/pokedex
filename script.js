2bb4de6eec34ff0dd5203c083a3044735be25e59
Andreas Burghardt <andreas-burghardt@gmx.net>
2022-10-07 11:44:23 +0200

    compact functions

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-
+//GLOBAL DECL
 let currentPokemon;
 let currPos = 0;
 let pokeList = [];
 let pokenames = [];
 let pokeTypes = ['Fire', 'Water', 'Grass', 'Electric', 'Poison', 'Bug', 'Flying', 'Fairy', 'Ground', 'Psychic', 'Fighting'];
 
+
 //FETCH POKEMON DATAS
 async function loadPokedex() {
-    for (let i = 0; i < 21; i++) {
+    for (let i = 0; i < 151; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}`;
         let response = await fetch(url);
         currentPokemon = await response.json();
         pokeList.push(currentPokemon)
-        renderCard(i);
+        renderCards(i);
         pokenames.push(currentPokemon.name);
     }
 }
 
 
-//GET DETAILS FUNCTIONS
+//GET BASE INFOS
 function getName(i) {
     let pokemon = pokeList[i];
     return capitalize(pokemon.name);
+}
+
+
+function getId(i) {
+    let number = i + 1;
+    if (number < 10) {
+        return `00${number}`
+    } else if (number >= 10 && number < 100) {
+        return `0${number}`
+    } else {
+        return number;
+    }
 }
 
 
@@ -44,31 +57,26 @@ function renderTypes(types) {
 }
 
 
-function getId(i) {
-    let number = i + 1;
-    if (number < 10) {
-        return `00${number}`
-    } else if (number >= 10 && number < 100) {
-        return `0${number}`
-    } else {
-        return number;
-    }
-}
-
-
-////////////////RENDER MINI-CARDS
-function renderCard(i) {
+////////////////RENDER CARDS
+function renderCards(i) {
     let types = getTypes(i);
     let cardBox = document.querySelector('#card-box');
     cardBox.innerHTML += /*html*/ `
     <div class="card" id="card-${i}" onclick="showEntry(${i})">
+    ${createSingleCard(i, types)}
+    </div>`
+    document.querySelector(`#card-${i}`).style.backgroundColor = bgColor(i);
+}
+
+
+function createSingleCard(i, types) {
+    let string = '';
+    string += /*html*/ `
         <span class="poke-id">#${getId(i)}</span>
         <h3>${getName(i)}</h3>
         ${renderTypes(types)}
-        <img class="img-small" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png" alt="">
-    </div>
-    `
-    document.querySelector(`#card-${i}`).style.backgroundColor = bgColor(i);
+        <img class="img-small" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png" alt="">`
+    return string;
 }
 
 
@@ -83,6 +91,101 @@ function bgColor(i) {
     }
     return color;
 }
+
+
+//CREATE POKEMON-ENTRY
+function showEntry(i) {
+    let types = getTypes(i);
+    document.querySelector('.layer').classList.remove('dis-none');
+    let container = document.querySelector('.poke-entry-container');
+    container.innerHTML = '';
+    container.innerHTML = /*html*/ `
+    <div id ="entry-headline-${i}" class="entry-headline" style="background: ${bgColor(i)}"></div>
+    <div id="entry-imgbox-${i}" class="entry-imgbox" style="background: ${bgColor(i)}"></div>
+    <div id="entry-content-${i}" class="entry-content"></div>`
+    renderHeadline(i);
+    renderImgBox(i, types);
+    renderContent(i);
+}
+
+
+function renderHeadline(i) {
+    document.querySelector(`#entry-headline-${i}`).innerHTML = /*html*/ `
+    <span id="entry-id">#${getId(i)}</span>
+    <div class="change-entry">
+        <span id="prev-entry" class="entry-slider" onclick="changeEntry(${i}, -1)"><i class="fa-solid fa-circle-chevron-left"></i></span>
+        <span id="next-entry" class="entry-slider" onclick="changeEntry(${i}, 1)"><i class="fa-solid fa-circle-chevron-right"></i></span>
+    </div>
+    <i id="close-entry" class="fa-solid fa-xmark" onclick="closeEntry()"></i>`
+}
+
+
+function renderImgBox(i, types) {
+    document.querySelector(`#entry-imgbox-${i}`).innerHTML = /*html*/ `
+     <h3>${getName(i)}</h3>
+    ${renderTypes(types)}
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png" alt="">`
+}
+
+
+function renderContent(i) {
+    document.querySelector(`#entry-content-${i}`).innerHTML = /*html*/ `
+        <nav id="entry-nav-${i}" class="entry-nav"></nav>
+        <div id="entry-main-${i}" class="entry-main"></div>`
+    renderNav(i);
+    renderMain(i);
+}
+
+
+function renderNav(i) {
+    document.querySelector(`#entry-nav-${i}`).innerHTML = /*html*/ `
+        <a href="#" class="nav-link">About</a>
+        <a href="#" class="nav-link  active-link">Stats</a>
+        <a href="#" class="nav-link">Evolution</a>
+        <a href="#" class="nav-link">Moves</a>`
+}
+
+
+//RENDER DETAILS
+function renderMain(i) {
+    let stats = getStats(i);
+    for (let j = 0; j < stats.length; j++) {
+        renderStats(i, j, stats);
+        renderBarLine(i, j, stats[j][1]);
+    }
+}
+
+
+function renderStats(i, j, stats) {
+    document.querySelector(`#entry-main-${i}`).innerHTML += /*html*/ `
+        <div class="stats-line">
+            <div class="stats-label">${stats[j][0]}</div>
+            <div class="stats-value">${stats[j][1]}</div>
+            <div class="stats-bar">
+                <div class="bar-line" id="bar-line-${i}-${j}"></div>
+            </div>
+        </div>`
+}
+
+
+function getStats(i) {
+    let values = [['HP'], ['Attack'], ['Defense'], ['Sp. Atk'], ['Sp. Def'], ['Speed']];
+    for (let j = 0; j < pokeList[j]['stats'].length; j++) {
+        let curStat = pokeList[i]['stats'][j];
+        values[j].push(curStat['base_stat']);
+    }
+    return values;
+}
+
+
+function renderBarLine(i, j, val) {
+    let barline = document.querySelector(`#bar-line-${i}-${j}`);
+    barline.style.width = `${val}%`;
+    if (val <= 50) {
+        barline.style.backgroundColor = 'red';
+    }
+}
+
 
 
 //GENERAL FUNCTIONS
@@ -104,7 +207,7 @@ function capitalize(s) {
 
 
 function scrollToPokemon(name) {
-    let i = pokenames.indexOf(name);
+    let i = pokenames.indexOf(name.toLowerCase());
     let card = document.querySelector(`#card-${i}`);
     let targetPos = card.offsetTop;
     window.scrollTo({
@@ -130,40 +233,7 @@ function enter() {
 
 function checkIfPokemon(name) {
     return pokenames.indexOf(name.toLowerCase()) > -1;
-}
 
-
-//CREATE POKEMON-ENTRY
-function showEntry(i) {
-    let types = getTypes(i);
-    document.querySelector('.layer').classList.remove('dis-none');
-    let container = document.querySelector('.poke-entry-container');
-    container.innerHTML = '';
-    container.innerHTML = /*html*/ `
-    <div class="entry-headline" style="background: ${bgColor(i)}">
-        <span id="entry-id">#${getId(i)}</span>
-        <div class="change-entry">
-            <span id="prev-entry" class="entry-slider" onclick="changeEntry(${i}, -1)"><i class="fa-solid fa-circle-chevron-left"></i></span>
-            <span id="next-entry" class="entry-slider" onclick="changeEntry(${i}, 1)"><i class="fa-solid fa-circle-chevron-right"></i></span>
-        </div>
-        <i id="close-entry" class="fa-solid fa-xmark" onclick="closeEntry()"></i>
-    </div>
-    <div id="entry-imgbox-${i}" class="entry-imgbox" style="background: ${bgColor(i)}">
-        <h3>${getName(i)}</h3>
-        ${renderTypes(types)}
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png" alt="">
-    </div>
-    <div id="entry-content-${i}" class="entry-content">
-        <nav id="entry-nav-${i}" class="entry-nav">
-            <a href="#" class="nav-link">About</a>
-            <a href="#" class="nav-link  active-link">Stats</a>
-            <a href="#" class="nav-link">Evolution</a>
-            <a href="#" class="nav-link">Moves</a>
-        </nav>
-        <div id="entry-main-${i}" class="entry-main"></div>
-    </div>
-    `
-    renderStats(i);
 }
 
 
@@ -179,42 +249,4 @@ function changeEntry(i, j) {
 
 function closeEntry() {
     document.querySelector('.layer').classList.add('dis-none');
-}
-
-//RENDER STATS
-function renderStats(i) {
-    let stats = getStats(i);
-    let content = document.querySelector(`#entry-main-${i}`);
-    content.innerHTML = '';
-    for (let j = 0; j < stats.length; j++) {
-        content.innerHTML += /*html*/ `
-        <div class="stats-line">
-            <div class="stats-label">${stats[j][0]}</div>
-            <div class="stats-value">${stats[j][1]}</div>
-            <div class="stats-bar">
-                <div class="bar-line" id="bar-line-${i}-${j}"></div>
-            </div>
-        </div>
-    `;
-        renderBarLine(i, j, stats[j][1]);
-    }
-}
-
-
-function getStats(i) {
-    let values = [['HP'], ['Attack'], ['Defense'], ['Sp. Atk'], ['Sp. Def'], ['Speed']];
-    for (let j = 0; j < pokeList[j]['stats'].length; j++) {
-        let curStat = pokeList[i]['stats'][j];
-        values[j].push(curStat['base_stat']);
-    }
-    return values;
-}
-
-
-function renderBarLine(i, j, val) {
-    let barline = document.querySelector(`#bar-line-${i}-${j}`);
-    barline.style.width = `${val}%`;
-    if (val <= 50) {
-        barline.style.backgroundColor = 'red';
-    }
 }
